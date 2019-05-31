1cf721d89dda535b78e254e50665940b503db0bb
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-05-31 10:46:35 -0700

    navbar in place styling it next

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MapContainer from './MapContainer';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Navbar/>
       </header>
       <div className="map-div">
         <MapContainer />
