58cb35195844f4bc70534ea69d4280674848a02d
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-05-31 11:41:41 -0700

    removed commented out code

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,11 +3,6 @@ import M from 'materialize-css';
 
 export default class Navbar extends Component {
 
-	/*componentDidMount() {
-		let elems = document.querySelectorAll('.dropdown-trigger');
-		M.Dropdown.init(elems, {inDuration: 300, outDuration: 225});
-	}*/
-
 	render() {
 		return(
 			  <nav>
@@ -28,19 +23,6 @@ export default class Navbar extends Component {
 				    <li><a href="#">About Us</a></li>
 			 	</ul>
 			  </nav>
-
-			/* <div className='input-field col s12'>
-				<a className='dropdown-button btn' data-activates='dropdown1'>Drop Me!</a>
-				<ul id='dropdown1' className
-				='dropdown-content'>
-					<li><a href='#!'>one</a></li>
-					<li><a href='#!'>two</a></li>
-					<li className='divider'></li>
-					<li><a href='#!'>three</a></li>
-					<li><a href="#!"><i className="material-icons">view_module</i>four</a></li>
-                 	<li><a href="#!"><i className="material-icons">cloud</i>five</a></li>
-				</ul>
-			</div>*/
 			)
 	}
 }
