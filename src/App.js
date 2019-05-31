be55ddc4df470c9f163ee01cbd8155d4f7a09b4f
Nathaniel Dominguez <nathaniel.a.dominguez@gmail.com>
2019-05-31 12:56:26 -0700

    moved the json commands

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 import MapContainer from './MapContainer';
 import Navbar from './Navbar';
 
+const dataFile = require('./events_formatted.json')
+const stringData = JSON.stringify(dataFile);
+console.log(stringData);
+const events = JSON.parse(stringData);
+
+console.log(events.name)
+
+
 function App() {
   return (
     <div className="App">
